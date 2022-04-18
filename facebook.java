410b08412c3b6e94bfa2538e3d150ad1fa226590
willyanggip <92911347+willyanggip@users.noreply.github.com>
2022-04-18 14:46:58 +0700

    Add files via upload

---
+++ facebook.java
@@ -0,0 +1,28 @@
+public class facebook {
+    private String name;
+    private String like;
+    private String comment;
+    private String share;
+
+public facebook (String gName, String gLike, String gComment, String gShare){
+    this.name       = gName;
+    this.like       = gLike;
+    this.comment    = gComment;
+    this.share      = gShare;
+}
+
+public int message (int jumlah){
+    message = message + jumlah;
+    return message;
+}
+public int Like (int jumlah){
+    Like = Like + jumlah;
+    return Like;
+}
+public int Share (int jumlah){
+    Share = Share + jumlah;
+    return saldo;
+}
+}
+
+
